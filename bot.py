8362f68bb2ed3cd7b0033eddbac127a88e82434c
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-04-23 17:13:55 -0400

    True initial commit, updated framework.

--- bot.py
+++ bot.py
@@ -1,23 +1,27 @@
 import os
-
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+TOKEN = os.getenv('TOKEN')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+	print(f"{client.user} initialized.")
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+	guildCount = 0
+
+	for guild in client.guilds:
+		print(f"Connected to {guild.name} ({guild.id}).")
+		guildCount = guildCount + 1
+
+	print(f"{guildCount} total connected servers.")
+
+@client.event
+async def on_message(message):
+	if message.content == "hello sobbot":
+		await message.channel.send("hi :pleading_face:")
 
 client.run(TOKEN)
