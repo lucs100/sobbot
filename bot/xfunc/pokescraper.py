34364026f66867cc635c4f897614cd4aa07bc095
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-04-24 16:47:18 -0400

    Wrote PokeAPI scraper. One time use.

---
+++ bot/xfunc/pokescraper.py
@@ -0,0 +1,29 @@
+import requests, json
+
+pkmn = {}
+pokeCount = 898
+# DO NOT PULL FROM THE API 898 TIMES UNLESS JSON SAVING WORKS THAT WOULD BE SO STUPID  
+
+for n in range(1, 4):
+    current = requests.get(f"https://pokeapi.co/api/v2/pokemon/{n}/")
+    current = json.loads(current.text)
+
+    nDictionary = {
+        "name": current["species"]["name"],
+        "art": current["sprites"]["other"]["official-artwork"]["front_default"],
+        "sprite": current["sprites"]["versions"]["generation-viii"]["icons"]["front_default"],
+        "typeCount": len(current["types"])
+    }
+
+    if nDictionary["typeCount"] == 1:
+        print("AAAAA")
+        nDictionary["type1"] = (current["types"][0]["type"]["name"])
+    elif nDictionary["typeCount"] == 2:
+        nDictionary["type1"] = (current["types"][0]["type"]["name"])
+        nDictionary["type2"] = (current["types"][1]["type"]["name"])
+
+    # print(json.dumps(nDictionary, indent=2))
+
+    pkmn[n] = nDictionary
+
+print(json.dumps(pkmn, indent=2))
