3479f906a9f809c7e3084a7f85bf74655292d1b2
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-05-07 16:27:46 -0400

    Implemented champion ID lookup

--- bot/riotapi.py
+++ bot/riotapi.py
@@ -6,6 +6,12 @@ RIOTTOKEN = os.getenv('RIOTTOKEN')
 headers = {"X-Riot-Token": RIOTTOKEN}
 url = "https://na1.api.riotgames.com"
 
+champs = {}
+
+with open('bot/func/data/champs.json') as f:
+    data = json.loads(f.read())
+    champs = data
+
 def parseSpaces(s):
     return s.replace(" ", "%20")
 
@@ -17,9 +23,8 @@ def checkKeyInvalid():
     return not (response.status_code != 401 and response.status_code != 403)
 
 def getSummonerData(s):
-    s = parseSpaces(s)
     response = requests.get(
-        (url + f"/lol/summoner/v4/summoners/by-name/{s}"),
+        (url + f"/lol/summoner/v4/summoners/by-name/{parseSpaces(s)}"),
         headers = headers
     )
     summonerData = json.loads(response.text)
@@ -28,11 +33,19 @@ def getSummonerData(s):
 def getESID(s): #encrypted summoner id
     if checkKeyInvalid():
         return False
-    return getSummonerData["accoundId"]
+    return getSummonerData(s)["id"]
 
 def getNameAndLevel(s):
     if checkKeyInvalid():
         return False
     summonerData = getSummonerData(s)
     return {"name": summonerData["name"], "level": int(summonerData["summonerLevel"])}
+
+def getTopMasteries(s):
+    response = requests.get(
+        (url + f"/lol/champion-mastery/v4/champion-masteries/by-summoner/{getESID(parseSpaces(s))}"),
+        headers = headers
+    )
+    h = response.json()
+    print(json.dumps(h, indent=2))
 
