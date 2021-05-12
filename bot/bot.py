63f83101e9e8ec2f51718bd69a3b1e88e4c0c131
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-05-12 16:15:16 -0400

    Removed unnecessary parentheses from await(message

--- bot/bot.py
+++ bot/bot.py
@@ -47,13 +47,13 @@ async def on_message(message):
 			await message.channel.send("hi :pleading_face:")
 			
 		if c == "ping":
-			await(message.channel.send("pong! ({0}ms)".format(int(client.latency*1000))))
+			await message.channel.send("pong! ({0}ms)".format(int(client.latency*1000)))
 
 		#special channels
 		if message.channel.id == 835388133959794699:
 			content = sob.parseMath(c)
 			if content != None:
-				await(message.channel.send(content))
+				await message.channel.send(content)
 
 		#prefixed messages
 		if c.startswith('s!'):
@@ -65,21 +65,21 @@ async def on_message(message):
 
 
 			if c == "flipcoin":
-				await(message.channel.send(sob.flipCoin()))
+				await message.channel.send(sob.flipCoin())
 
 			if c.startswith('d'):
 				try:
 					n = (int(c[1:]))
-					await(message.channel.send(sob.die(n)))
+					await message.channel.send(sob.die(n))
 				except:
 					pass
 
 			if c == "sobbleimage":
-				await(message.channel.send(file = sob.sobbleImage()))
+				await message.channel.send(file = sob.sobbleImage())
 
 			if c.startswith("math"):
 				content = sob.parseMath(c[4:].strip())
-				await(message.channel.send(content))
+				await message.channel.send(content)
 			
 			if c.startswith("pkmn"):
 				data = c[4:].strip()      #query
@@ -90,19 +90,19 @@ async def on_message(message):
 				finally:
 					embed = sob.pkmnLookup(data)
 					if embed != None: 	  #result found
-						await(message.channel.send(embed=sob.pkmnLookup(data)))
+						await message.channel.send(embed=sob.pkmnLookup(data))
 			
 			if c == "uptime":
-				await(message.channel.send(sob.timeRunning(startTime)))
+				await message.channel.send(sob.timeRunning(startTime))
 
 			if c == "starttime":
 				startingTime = datetime.fromtimestamp(startTime).strftime("%B %d at %I:%M %p")
-				await(message.channel.send(f"Sobbot has been online since {startingTime}!"))
+				await message.channel.send(f"Sobbot has been online since {startingTime}!")
 				#maybe format this a little nicer?
 			
 			if c == "randblue" or c == "randbluw":
 				fp, content = (sob.randomBlue())
-				await(message.channel.send(file=fp, content=content))
+				await message.channel.send(file=fp, content=content)
 			
 			if c.startswith("link"):
 				try:
@@ -124,16 +124,16 @@ async def on_message(message):
 					if name.strip() == "":
 						name = riotapi.isUserRegistered(message.author.id) #bool or sname
 						if name == False:
-							await(message.channel.send(f"<@!{message.author.id}>, you aren't registered! Use s!lolregister to add your summoner name. You can also specify a summoner name after this command to use it while unregistered."))
+							await message.channel.send(f"<@!{message.author.id}>, you aren't registered! Use s!lolregister to add your summoner name. You can also specify a summoner name after this command to use it while unregistered.")
 							return True
 					data = riotapi.getNameAndLevel(name)
 					if data == False:
-						await(message.channel.send("Key expired."))
+						await message.channel.send("Key expired.")
 						return True
 					name, level = data["name"], data["level"]
-					await(message.channel.send(f"Summoner **{name}** is level **{level}**."))
+					await message.channel.send(f"Summoner **{name}** is level **{level}**.")
 				# except:
-				# 	await(message.channel.send(f"Summoner **{name}** doesn't exist."))
+				# 	await message.channel.send(f"Summoner **{name}** doesn't exist.")
 				# 	return True
 
 			if c.startswith("lolmastery"):
@@ -142,15 +142,15 @@ async def on_message(message):
 					if name.strip() == "":
 						name = riotapi.isUserRegistered(message.author.id) #bool or sname
 						if name == False:
-							await(message.channel.send(f"<@!{message.author.id}>, you aren't registered! Use s!lolregister to add your summoner name. You can also specify a summoner name after this command to use it while unregistered."))
+							await message.channel.send(f"<@!{message.author.id}>, you aren't registered! Use s!lolregister to add your summoner name. You can also specify a summoner name after this command to use it while unregistered.")
 							return True
 					embed = riotapi.embedTopMasteries(name)
 					if embed == False:
-						await(message.channel.send("Key expired."))
+						await message.channel.send("Key expired.")
 						return True
-					await(message.channel.send(embed=embed))
+					await message.channel.send(embed=embed)
 				except:
-					await(message.channel.send(f"Summoner **{name}** doesn't exist, or your key expired. Try again!"))
+					await message.channel.send(f"Summoner **{name}** doesn't exist, or your key expired. Try again!")
 					return True
 			
 			if c.startswith("lolregister"):
@@ -165,9 +165,9 @@ async def on_message(message):
 				else:
 					ok = riotapi.editRegistration(id, name)
 				if ok != False:
-					await(message.channel.send(f"Set <@!{id}>'s summoner name to **{ok}**!"))
+					await message.channel.send(f"Set <@!{id}>'s summoner name to **{ok}**!")
 				else:
-					await(message.channel.send(f"Summoner **{name}** doesn't exist, or your key expired. Try again!"))
+					await message.channel.send(f"Summoner **{name}** doesn't exist, or your key expired. Try again!")
 				return True
 
 			if c.startswith("lolrank"):
@@ -176,15 +176,15 @@ async def on_message(message):
 					if name.strip() == "":
 						name = riotapi.isUserRegistered(message.author.id) #bool or sname
 						if name == False:
-							await(message.channel.send(f"<@!{message.author.id}>, you aren't registered! Use s!lolregister to add your summoner name. You can also specify a summoner name after this command to use it while unregistered."))
+							await message.channel.send(f"<@!{message.author.id}>, you aren't registered! Use s!lolregister to add your summoner name. You can also specify a summoner name after this command to use it while unregistered.")
 							return True
 					embed = riotapi.embedRankedData(name)
 					if embed == False:
-						await(message.channel.send("Key expired."))
+						await message.channel.send("Key expired.")
 						return True
-					await(message.channel.send(embed=embed))
+					await message.channel.send(embed=embed)
 				except:
-					await(message.channel.send(f"Summoner **{name}** doesn't exist, or your key expired. Try again!"))
+					await message.channel.send(f"Summoner **{name}** doesn't exist, or your key expired. Try again!")
 					return True
 
 			if c == "lolapireload":
@@ -205,9 +205,9 @@ async def on_message(message):
 			if c == "coinstart":
 				ok = coin.addRegistration(message.author.id)
 				if ok:
-					await(message.channel.send(f"<@!{message.author.id}> added to soblecoin! You have {coin.getUserCoins(message.author.id)} coins."))
+					await message.channel.send(f"<@!{message.author.id}> added to soblecoin! You have {coin.getUserCoins(message.author.id)} coins.")
 					return True
-					await(message.channel.send(f"<@!{message.author.id}> , you already have soblecoins! You have {coin.startingCoins} coins."))
+				await message.channel.send(f"<@!{message.author.id}> , you already have soblecoins! You have {coin.startingCoins} coins.")
 				return False
 			
 			if c.startswith("give"):
@@ -228,7 +228,7 @@ async def on_message(message):
 				if ok == 4:
 					await message.channel.send(f"You can't send coins to yourself!")
 				# except:
-					# await(message.channel.send("Use s!give (recipient) (value) to send a friend soblecoins!"))
+					# await message.channel.send("Use s!give (recipient) (value) to send a friend soblecoins!")
 				return True
 			
 			if c == "balance":
