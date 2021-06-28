a4466f99f6e3453119774ba6d01586a9a972310d
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-06-28 17:54:31 -0400

    Implemented basic functions

--- bot/finance.py
+++ bot/finance.py
@@ -1,7 +1,42 @@
 import yfinance as yf
+from datetime import datetime
 
-gme = yf.Ticker("GME")
+#class??????
 
-print(gme.info["regularMarketPrice"])
-print("\n\n\n")
-print(gme.history(period="1d"))
+def getMarketPhase(now = datetime.now()):
+    preOpen = now.replace(hour=3, minute=30, second=0, microsecond=0)
+    marketOpen = now.replace(hour=9, minute=30, second=0, microsecond=0)
+    marketClose = now.replace(hour=16, minute=0, second=0, microsecond=0)
+    afterHourClose = now.replace(hour=20, minute=0, second=0, microsecond=0)
+
+    if (now < preOpen):
+        return("Market closed")
+    elif (now < marketOpen):
+        return("Premarket")
+    elif (now < marketClose):
+        return("Market open")
+    elif (now < afterHourClose):
+        return("After hours")
+    else:
+        return("Market closed")
+
+def getTicker(symbol):
+    return yf.Ticker(symbol)
+
+def getBasicData(ticker):
+    data = dict([])
+    data["symbol"] = ticker.info["symbol"]
+    data["name"] = ticker.info["shortName"]
+    data["price"] = float(ticker.info["regularMarketPrice"])
+    data["open"] = float(ticker.info["open"])
+    data["change"] = round(data["price"] - data["open"], 2)
+    data["changePercent"] = round(((data["change"]*100)/data["open"]), 2)
+    data["volume"] = int(ticker.info["volume"])
+    data["avgVolume"] = int(ticker.info["averageVolume"])
+    data["relativeVolume"] = data["volume"] / data["avgVolume"]
+    return data
+
+gme = getTicker("gme")
+print(getBasicData(gme))
+
+print(getMarketPhase())
