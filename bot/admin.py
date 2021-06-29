8b5784772c29ae23762e3fb750016e881f3221e5
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-06-29 14:34:19 -0400

    Added prefix set/reset methods

--- bot/admin.py
+++ bot/admin.py
@@ -21,3 +21,29 @@ def updateGuildData():
     with open('bot/resources/data/guilds.json', 'w') as fp:
         json.dump(guilds, fp, indent=4)
     return True
+
+def resetGuildPrefix(id):
+    id = str(id)
+    try:
+        if "prefix" in guilds[id]:
+            guilds[id].pop("prefix")
+            updateGuildData()
+            return "ok"
+        else:
+            return "nop"
+    except:
+        return "error"
+
+def updateGuildPrefix(id, prefix):
+    id = str(id)
+    if prefix == DefaultPrefix:
+        resetGuildPrefix(id)
+        return True
+    if id in guilds:
+        guilds[id]["prefix"] = ""
+    else:
+        guilds[id] = {}
+        guilds[id]["prefix"] = ""
+    guilds[id]["prefix"] = prefix
+    updateGuildData()
+    return (guilds[id]["prefix"] == prefix)
