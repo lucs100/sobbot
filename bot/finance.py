af61cd506b98fc98b2f30047df88850d663f03f8
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-06-29 00:48:35 -0400

    Added safe Stock constructor

--- bot/finance.py
+++ bot/finance.py
@@ -5,16 +5,26 @@ from datetime import datetime
 class Stock:
     def __init__(self, symbol):
         data = yf.Ticker(symbol)
-        self.symbol = data.info["symbol"]
-        self.name = data.info["shortName"]
-        self.currentPrice = float(data.info["regularMarketPrice"])
-        self.openPrice = float(data.info["open"])
-        self.change = round(self.currentPrice - self.openPrice, 2)
-        self.changePercent = round(((self.change*100)/self.openPrice), 2)
-        self.volume = int(data.info["volume"])
-        self.averageVolume = int(data.info["averageVolume"])
-        self.relativeVolume = self.volume / self.averageVolume
+        try:
+            self.symbol = data.info["symbol"]
+            self.name = data.info["shortName"]
+            self.currentPrice = float(data.info["regularMarketPrice"])
+            self.openPrice = float(data.info["open"])
+            self.change = round(self.currentPrice - self.openPrice, 2)
+            self.changePercent = round(((self.change*100)/self.openPrice), 2)
+            self.volume = int(data.info["volume"])
+            self.averageVolume = int(data.info["averageVolume"])
+            self.relativeVolume = self.volume / self.averageVolume
+        except KeyError:
+            raise ValueError(f"The symbol {symbol} doesn't exist.")
 
+def safelyCreateStock(symbol):
+    data = yf.Ticker(symbol)
+    try:
+        temp = data.info["symbol"]
+    except KeyError:
+        return None
+    return Stock(symbol)
 
 def getMarketPhase(now = datetime.now()):
     preOpen = now.replace(hour=3, minute=30, second=0, microsecond=0)
@@ -23,17 +33,12 @@ def getMarketPhase(now = datetime.now()):
     afterHourClose = now.replace(hour=20, minute=0, second=0, microsecond=0)
 
     if (now < preOpen):
-        return("Market closed")
+        return 0 # Market closed
     elif (now < marketOpen):
-        return("Premarket")
+        return 1 # Premarket
     elif (now < marketClose):
-        return("Market open")
+        return 2 # Market open
     elif (now < afterHourClose):
-        return("After hours")
+        return 3 # After hours
     else:
-        return("Market closed")
-
-gme = Stock("gme")
-print(gme)
-
-print(getMarketPhase())
+        return 4 # Market closed
