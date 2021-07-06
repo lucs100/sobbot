dfcf886fd0a046e9ecc5961373afa0f1e3dc90b0
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-07-05 20:28:33 -0400

    Added lockout backup to avoid unneccesary requests

--- bot/resources/functions/pokescraper.py
+++ bot/resources/functions/pokescraper.py
@@ -2,37 +2,39 @@ import requests, json
 
 pkmn = {}
 pokeCount = 898
+LockOutFlag = False # set this to true to allow this file to run
 # DO NOT PULL FROM THE API 898 TIMES UNLESS JSON SAVING WORKS THAT WOULD BE SO STUPID  
 
-for n in range(1, pokeCount+1):
-    current = requests.get(f"https://pokeapi.co/api/v2/pokemon/{n}/")
-    current = json.loads(current.text)
+if LockOutFlag:
+    for n in range(1, pokeCount+1):
+        current = requests.get(f"https://pokeapi.co/api/v2/pokemon/{n}/")
+        current = json.loads(current.text)
 
-    nDictionary = {
-        "name": current["species"]["name"],
-        "art": current["sprites"]["other"]["official-artwork"]["front_default"],
-        "sprite": current["sprites"]["versions"]["generation-viii"]["icons"]["front_default"],
-        "typeCount": len(current["types"])
-    }
+        nDictionary = {
+            "name": current["species"]["name"],
+            "art": current["sprites"]["other"]["official-artwork"]["front_default"],
+            "sprite": current["sprites"]["versions"]["generation-viii"]["icons"]["front_default"],
+            "typeCount": len(current["types"])
+        }
 
-    if nDictionary["typeCount"] == 1:
-        nDictionary["type1"] = (current["types"][0]["type"]["name"])
-    elif nDictionary["typeCount"] == 2:
-        nDictionary["type1"] = (current["types"][0]["type"]["name"])
-        nDictionary["type2"] = (current["types"][1]["type"]["name"])
+        if nDictionary["typeCount"] == 1:
+            nDictionary["type1"] = (current["types"][0]["type"]["name"])
+        elif nDictionary["typeCount"] == 2:
+            nDictionary["type1"] = (current["types"][0]["type"]["name"])
+            nDictionary["type2"] = (current["types"][1]["type"]["name"])
 
-    # print(json.dumps(nDictionary, indent=2))
+        # print(json.dumps(nDictionary, indent=2))
 
-    pkmn[n] = nDictionary
+        pkmn[n] = nDictionary
 
-# print(json.dumps(pkmn, indent=2))
+    # print(json.dumps(pkmn, indent=2))
 
-with open('bot/resources/data/pkmn.json', 'w', encoding="utf-8") as f:
-    json.dump(pkmn, f, ensure_ascii=False, indent=4)
+    with open('bot/resources/data/pkmn.json', 'w', encoding="utf-8") as f:
+        json.dump(pkmn, f, ensure_ascii=False, indent=4)
 
-count = len(pkmn)
+    count = len(pkmn)
 
-if count == pokeCount:
-    print(f"Complete - {count}/{pokeCount}.")
-else:
-    print(f"Failed - {count}/{pokeCount}.")
+    if count == pokeCount:
+        print(f"Complete - {count}/{pokeCount}.")
+    else:
+        print(f"Failed - {count}/{pokeCount}.")
