1d2881320239a2c1b3e009f199af2a94e246cc52
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-07-09 16:03:34 -0400

    Improved efficiency, randomized order

--- bot/matchPullScript.py
+++ bot/matchPullScript.py
@@ -1,5 +1,6 @@
 import riotapi as r
 from time import sleep
+import random 
 
 def clear(message):
     print("\n" * 100)
@@ -7,10 +8,16 @@ def clear(message):
 
 def script(additionals):
     mList = additionals + r.summonerList
+    random.shuffle(mList)
     while True:
         for summoner in mList:
             clear("SCRIPT RUNNING! Do not kill this script, you could corrupt the match table.")
+            print(f"Current user: {summoner}")
+            sleep(10)
             r.bulkPullMatchData(r.getMatchHistory(summoner))
             sleep(3)
             clear("Script on cooldown. It is safe to kill the script.")
-            sleep(180)
+            print(f"Current matches: {len(r.pulledMatches)}")
+            sleep(120)
+
+script()
