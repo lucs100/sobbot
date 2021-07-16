8b9114b0ffe3b8d243d275b78a42d178fcd36584
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-07-15 21:41:33 -0400

    Improved script interface + name input

--- bot/matchPullScript.py
+++ bot/matchPullScript.py
@@ -14,13 +14,22 @@ def script(additionals):
         for summoner in mList:
             clear("SCRIPT RUNNING! Do not kill this script, you could corrupt the match table.")
             print(f"Current user: {summoner}")
-            sleep(10)
+            sleep(3)
             r.bulkPullMatchData(r.getMatchHistory(summoner))
             sleep(3)
             clear("Script on cooldown. It is safe to kill the script.")
-            print(f"Current matches: {len(r.pulledMatches)}")
+            print(f"Current matches: {len(r.pulledMatches)} ({beforeCycle})")
             sleep(120)
         if len(r.pulledMatches) == beforeCycle: # max pulled
+            print("Pulled all matches! Exiting.")
             exit()
 
-script()
+additionalNames = []
+while True:
+    name = input("Enter a name to add it to the list. Press Enter to start.")
+    if name == "":
+        break
+    else:
+        additionalNames.append(name)
+
+script(additionalNames)
