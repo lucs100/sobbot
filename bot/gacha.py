088306038a1b8aeed6d4ff787b3618ab42da2009
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-08-18 14:24:56 -0400

    Added dummy functions and classes

--- bot/gacha.py
+++ bot/gacha.py
@@ -1,21 +1,36 @@
 
-import json, random
+import json, random, datetime
 
 gachaDict = dict()
 
-class GachaItem:
+class OwnedGachaItem:
+    def __init__(self, stockItem, user):
+        pass
+
+class StockGachaItem:
     def __init__(self, data):
         self.name = data["name"]
         self.rarity = data["rarity"]
         self.baseValue = data["value"]
+    
+    def getUserItemInstances(self, userID):
+        # return all OGIs of a certain item class
+        pass
+    
+    def createOwnedItem(self, userID):
+        # generate random values for value, quality, attack/defense?????? other stats
+        # 
+        pass
+
 
 with open('bot/resources/data/gachaList.json') as f:
     data = list(json.loads(f.read())) # unpacking data
     for item in data:
-        item = GachaItem(item)
+        item = StockGachaItem(item)
         gachaDict[item.name] = item
-    print(gachaDict)
     f.close()
+
+
 
 # with open('bot/resources/data/gachaStats.json') as f:
 #     data = json.loads(f.read()) # unpacking data
